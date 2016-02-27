d64b1836c0666c450807dced143601fc48b0c41d
Tom Dooner <tom.dooner@brigade.com>
2016-02-27 00:23:59 -0800

    Remove shared_examples_for spec group
    This group wasn't actually being helpful, and so I replaced it with a
    couple tests that explicitly check the setting/getting/deleting around
    the flush and re-instantiation.

--- spec/sstable_spec.rb
+++ spec/sstable_spec.rb
@@ -2,7 +2,10 @@ require 'rspec'
 require 'tmpdir'
 require_relative '../sstable'
 
-shared_examples_for 'basic kv operations' do
+describe 'SSTable from scratch' do
+  around { |ex| Dir.mktmpdir { |d| @tmpdir = d; ex.run } }
+  subject { SSTable.new(@tmpdir) }
+
   it 'allows key-value get/set' do
     subject.set 'key-1', 'value-1'
     expect(subject.get 'key-1').to eq('value-1')
@@ -29,13 +32,6 @@ shared_examples_for 'basic kv operations' do
     subject.delete 'foo'
     expect(subject.get 'foo').to be_nil
   end
-end
-
-describe 'SSTable from scratch' do
-  around { |ex| Dir.mktmpdir { |d| @tmpdir = d; ex.run } }
-  subject { SSTable.new(@tmpdir) }
-
-  it_behaves_like 'basic kv operations'
 
   it 'flushes to disk' do
     subject.set 'foo', 'bar'
@@ -54,11 +50,37 @@ describe 'SSTable from scratch' do
     subject.flush
     expect(subject.get('foo')).to be_nil
   end
+end
 
-  describe 'when loaded from disk' do
-    around { |ex| Dir.mktmpdir { |d| @tmpdir = d; ex.run } }
-    subject { SSTable.new(@tmpdir) }
+describe 'SSTable merging/flushing' do
+  around { |ex| Dir.mktmpdir { |d| @tmpdir = d; ex.run } }
 
-    it_behaves_like 'basic kv operations'
+  it 'persists values once flushed' do
+    table = SSTable.new(@tmpdir)
+    table.set 'foo', 'bar'
+    table.set 'bar', 'baz'
+    table.flush
+
+    table2 = SSTable.new(@tmpdir)
+    expect(table2.get('foo')).to eq('bar')
+    table2.set 'baz', 'bonkers'
+    table2.flush
+
+    table3 = SSTable.new(@tmpdir)
+    expect(table3.get 'baz').to eq('bonkers')
+  end
+
+  it 'deletes values when flushed' do
+    table = SSTable.new(@tmpdir)
+    table.set 'foo', 'bar'
+    table.flush
+
+    table2 = SSTable.new(@tmpdir)
+    expect(table2.get('foo')).to eq('bar')
+    table2.delete('foo')
+    table2.flush
+
+    table3 = SSTable.new(@tmpdir)
+    expect(table3.get('foo')).to be_nil
   end
 end
